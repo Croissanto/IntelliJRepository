dff37e3cc3f7e0c633e64aa1d0b746827b6e4b87
Raffaele <antoniopapaleo24@gmail.com>
2023-03-02 16:11:29 +0100

    ModelMapper save

--- mygym/src/main/java/it/corso/mygym/service/UserService.java
+++ mygym/src/main/java/it/corso/mygym/service/UserService.java
@@ -1,12 +1,15 @@
 package it.corso.mygym.service;
 
 import it.corso.mygym.model.User;
+import it.corso.mygym.model.dto.UserDto;
 
 public interface UserService {
 
-    User save(User user);
+    User save(UserDto user);
 
-    User findById(Long id);
+    User findById(long id);
 
-    boolean deleteById(Long id);
+    boolean deleteById(long id);
+
+    User update(long id, UserDto user);
 }
--- mygym/src/main/java/it/corso/mygym/service/UserServiceImpl.java
+++ mygym/src/main/java/it/corso/mygym/service/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package it.corso.mygym.service;
 
 import it.corso.mygym.model.User;
+import it.corso.mygym.model.dto.UserDto;
 import it.corso.mygym.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,24 +18,21 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    public User save(User user) {
+    public User save(UserDto dto) {
+        ModelMapper mapper = new ModelMapper();
+        User user = mapper.map(dto, User.class);
         return userRepo.save(user);
     }
 
     @Override
-    public User findById(Long id) {
+    public User findById(long id) {
 
         Optional<User> opt = userRepo.findById(id);
-        if (opt.isPresent()) {
-
-            return opt.get();
-
-        }
-        return null;
+        return opt.orElse(null);
     }
 
     @Override
-    public boolean deleteById(Long id) {
+    public boolean deleteById(long id) {
         try {
             userRepo.deleteById(id);
             return true;
@@ -41,4 +41,20 @@ public class UserServiceImpl implements UserService {
             return false;
         }
     }
+
+    @Override
+    public User update(long id, UserDto dto) {
+        Optional<User> opt = userRepo.findById(id);
+        dto.setId(id);
+        if(opt.isPresent()) {
+        User user = opt.get();
+        dto.setId(id);
+        return userRepo.saveAndFlush(user);
+
+        }
+
+         else throw new ResourceNotFoundException();
+    }
+
+
 }
