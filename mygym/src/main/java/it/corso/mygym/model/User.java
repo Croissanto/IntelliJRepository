18641e64fd6b191d71a5e245349691bb988bf086
Raffaele <antoniopapaleo24@gmail.com>
2023-03-02 10:19:21 +0100

    Creazione user

---
+++ mygym/src/main/java/it/corso/mygym/model/User.java
@@ -0,0 +1,19 @@
+package it.corso.mygym.model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class User {
+
+
+    private long Id;
+
+    private String name;
+
+    private String surname;
+
+
+}
